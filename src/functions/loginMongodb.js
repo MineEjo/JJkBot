a232b33411cc9fb629a9485ab74d9c07dfa11c67
MineEjo <evgenyshaimov@gmail.com>
2022-02-04 00:31:57 +0300

    All Events Collected One Folder.

--- src/functions/loginMongodb.js
+++ src/functions/loginMongodb.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const fs = require('fs');
-const mongoEventsFiles = fs.readdirSync('../src/mongoEvents').filter(file => file.endsWith('.js'));
+const mongoEventsFiles = fs.readdirSync('../src/events/mongodb').filter(file => file.endsWith('.js'));
 
 module.exports = (client) => {
     client.loginMongodb = async () => {
         for (let file of mongoEventsFiles) {
-            const event = require(`../mongoEvents/${file}`);
+            const event = require(`../events/mongodb/${file}`);
             if (event.once) {
                 mongoose.connection.once(event.name, (...args) => event.execute(...args));
             } else {
