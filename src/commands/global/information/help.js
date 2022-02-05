3ce2b98d216317d5593e96104e0ab55a9f0248b4
MineEjo <evgenyshaimov@gmail.com>
2022-02-05 16:57:25 +0300

    Variable Names Changed => Numbers.

--- src/commands/global/information/help.js
+++ src/commands/global/information/help.js
@@ -5,22 +5,24 @@ const noneTranslate = require(`../../../translation/${config.bot.lang}.json`);
 const language = require('../../../functions/handleLanguages');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName(noneTranslate.commands.help.slash.name)
-        .setDescription(noneTranslate.commands.help.slash.description),
-    async execute(interaction) {
-        const translate = require(`../../../translation/${language(interaction.guild)}.json`);
-        const embed = new MessageEmbed()
-            .setTitle(translate.commands.help.title)
-            .setDescription(translate.commands.help.description.replace('${guildInvite}', config.guild.invite))
-            .addFields({
-                name: translate.commands.help.fields[0].name,
-                value: translate.commands.help.fields[0].value
-                    .replace('${githubLink}', config.bot.github)
-                    .replace('${guildInvite}', config.guild.invite),
-                inline: false
-            })
-            .setColor(config.color.primary);
-        await interaction.reply({embeds: [embed], ephemeral: true}).catch(console.error);
-    },
+	data: new SlashCommandBuilder()
+	.setName(noneTranslate.commands.help.slash.name)
+	.setDescription(noneTranslate.commands.help.slash.description),
+	async execute(interaction) {
+
+
+		const translate = require(`../../../translation/${language(interaction.guild)}.json`);
+		const embed = new MessageEmbed()
+		.setTitle(translate.commands.help.title)
+		.setDescription(translate.commands.help.description.replace('${1}', config.guild.invite))
+		.addFields({
+			name: translate.commands.help.fields[0].name,
+			value: translate.commands.help.fields[0].value
+			.replace('${2}', config.bot.github)
+			.replace('${1}', config.guild.invite),
+			inline: false
+		})
+		.setColor(config.color.primary);
+		await interaction.reply({embeds: [embed], ephemeral: true}).catch(console.error);
+	}
 };
--- src/commands/global/information/stats.js
+++ src/commands/global/information/stats.js
@@ -5,22 +5,22 @@ const config = require("../../../data/config.json");
 const noneTranslate = require(`../../../translation/${config.bot.lang}.json`);
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName(noneTranslate.commands.stats.slash.name)
-        .setDescription(noneTranslate.commands.stats.slash.description),
-    async execute(interaction) {
-        const translate = require(`../../../translation/${language(interaction.guild)}.json`);
+	data: new SlashCommandBuilder()
+	.setName(noneTranslate.commands.stats.slash.name)
+	.setDescription(noneTranslate.commands.stats.slash.description),
+	async execute(interaction) {
+		const translate = require(`../../../translation/${language(interaction.guild)}.json`);
 
-        const embed = new MessageEmbed()
-            .setTitle(translate.commands.stats.title)
-            .setDescription(translate.commands.stats.description
-                .replace('${clientPing}', interaction.client.ws.ping)
-                .replace('${clientReadyTimestamp}', parseInt(interaction.client.readyTimestamp / 1000))
-                .replace('${guildsSize}', interaction.client.guilds.cache.size)
-                .replace('${usersSize}', interaction.client.users.cache.size)
-                .replace('${channelsSize}', interaction.client.channels.cache.size))
-            .setColor(config.color.embed);
+		const embed = new MessageEmbed()
+		.setTitle(translate.commands.stats.title)
+		.setDescription(translate.commands.stats.description
+		.replace('${1}', interaction.client.ws.ping)
+		.replace('${2}', parseInt(interaction.client.readyTimestamp / 1000))
+		.replace('${3}', interaction.client.guilds.cache.size)
+		.replace('${4}', interaction.client.users.cache.size)
+		.replace('${5}', interaction.client.channels.cache.size))
+		.setColor(config.color.embed);
 
-        await interaction.reply({embeds: [embed], ephemeral: true}).catch(console.error);
-    },
+		await interaction.reply({embeds: [embed], ephemeral: true}).catch(console.error);
+	}
 };
