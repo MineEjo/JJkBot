4a6b73cf5c95329effadab75d1c874924d3b3b57
MineEjo <evgenyshaimov@gmail.com>
2022-02-05 17:16:01 +0300

    Load Handler Guilds,

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,12 +1,14 @@
 const {loadLanguages} = require('../functions/handleLanguages');
+const {loadGuilds} = require('../functions/mongodb/handleGuilds');
 
 module.exports = {
-    name: 'ready',
-    once: true,
-    async execute(client) {
-        await loadLanguages(client);
-        console.log(`[Bot] Ready! Logged in as ${client.user.tag}`);
+	name: 'ready',
+	once: true,
+	async execute(client) {
+		await loadLanguages(client);
+		await loadGuilds(client);
+		console.log(`[Bot] Ready! Logged in as ${client.user.tag}`);
 
-        client.user.setPresence({activities: [{name: `/help`, type: `PLAYING`}], status: "online"});
-    },
+		client.user.setPresence({activities: [{name: `/help`, type: `PLAYING`}], status: 'online'});
+	}
 };
