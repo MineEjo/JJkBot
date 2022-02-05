4fa2f6f2ca412430d4ff4a9f2214fa7f4a91c883
MineEjo <evgenyshaimov@gmail.com>
2022-02-05 17:13:03 +0300

    Added New Function.

---
+++ src/functions/lites/createWebhook.js
@@ -0,0 +1,18 @@
+const createWebhook = async (guild, channel) => {
+	let webhook;
+	await guild.fetchWebhooks().then(webhooks => {
+		webhook = webhooks.find(
+			(wh) => wh.channelId === channel.id && wh.name === guild.me.user.username
+		);
+	}).catch(console.error);
+
+	if (!webhook) {
+		await channel.createWebhook(guild.me.user.username, {
+			avatar: guild.me.user.avatarURL({dynamic: true})
+		}).then(wh => webhook = wh).catch(console.error);
+	}
+
+	return webhook;
+};
+
+module.exports.createWebhook = createWebhook;
