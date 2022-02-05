01cace8dfa6ae91ce165e14f8468987256725a6b
MineEjo <evgenyshaimov@gmail.com>
2022-02-05 17:06:04 +0300

    Added Ability Set Access.

--- src/commands/global/information/lang.js
+++ src/commands/global/information/lang.js
@@ -8,84 +8,83 @@ const language = require('../../../functions/handleLanguages');
 const {setLanguage} = require("../../../functions/handleLanguages");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName(noneTranslate.commands.lang.slash.name)
-        .setDescription(noneTranslate.commands.lang.slash.description),
-    async execute(interaction) {
-        const translate = require(`../../../translation/${language(interaction.guild)}.json`);
+	data: new SlashCommandBuilder()
+	.setName(noneTranslate.commands.lang.slash.name)
+	.setDescription(noneTranslate.commands.lang.slash.description),
+	restriction: config.FLAGS.CHANNEL,
+	async execute(interaction) {
+		const translate = require(`../../../translation/${language(interaction.guild)}.json`);
 
-        let embed = null;
-        let disabled = false;
-        if (interaction.user.id === interaction.guild.ownerId) {
-            embed = new MessageEmbed()
-                .setTitle(translate.commands.lang.title)
-                .setDescription(translate.commands.lang.description)
-                .setColor(config.color.embed);
-        } else {
-            disabled = true;
-            embed = new MessageEmbed()
-                .setTitle(translate.commands.lang.title)
-                .setDescription(translate.commands.lang.rights)
-                .setColor(config.color.error);
-        }
+		let embed = null;
+		let disabled = false;
+		if (interaction.user.id === interaction.guild.ownerId) {
+			embed = new MessageEmbed()
+			.setTitle(translate.commands.lang.title)
+			.setDescription(translate.commands.lang.description)
+			.setColor(config.color.embed);
+		} else {
+			disabled = true;
+			embed = new MessageEmbed()
+			.setTitle(translate.commands.lang.title)
+			.setDescription(translate.commands.lang.rights)
+			.setColor(config.color.error);
+		}
 
-        const options = require(`../../../translation/none.json`);
+		const options = require(`../../../translation/none.json`);
 
-        const row = new MessageActionRow()
-            .addComponents(
-                new MessageSelectMenu()
-                    .setDisabled(disabled)
-                    .setCustomId(ids.commands.lang.menu_choose_lang)
-                    .setPlaceholder(translate.commands.lang.placeholder)
-                    .addOptions([
-                        {
-                            label: options.commands.lang.options[0].label,
-                            value: options.commands.lang.options[0].value,
-                            description: options.commands.lang.options[0].description,
-                            emoji: options.commands.lang.options[0].emoji
-                        },
-                        {
-                            label: options.commands.lang.options[1].label,
-                            value: options.commands.lang.options[1].value,
-                            description: options.commands.lang.options[1].description,
-                            emoji: options.commands.lang.options[1].emoji
-                        }
-                    ])
-            );
+		const row = new MessageActionRow()
+		.addComponents(
+			new MessageSelectMenu()
+			.setDisabled(disabled)
+			.setCustomId(ids.commands.lang.menu_choose_lang)
+			.setPlaceholder(translate.commands.lang.placeholder)
+			.addOptions([
+				{
+					label: options.commands.lang.options[0].label,
+					value: options.commands.lang.options[0].value,
+					description: options.commands.lang.options[0].description,
+					emoji: options.commands.lang.options[0].emoji
+				},
+				{
+					label: options.commands.lang.options[1].label,
+					value: options.commands.lang.options[1].value,
+					description: options.commands.lang.options[1].description,
+					emoji: options.commands.lang.options[1].emoji
+				}
+			])
+		);
 
-        await interaction.reply({fetchReply: true, embeds: [embed], ephemeral: true, components: [row]})
-            .then((message) => {
-                const collector = message.createMessageComponentCollector({componentType: 'SELECT_MENU', time: 20000});
+		await interaction.reply({fetchReply: true, embeds: [embed], ephemeral: true, components: [row]})
+		.then((message) => {
+			const collector = message.createMessageComponentCollector({componentType: 'SELECT_MENU', time: 20000});
 
-                collector.on('collect', async i => {
-                    if (i.user.id === interaction.user.id) {
-                        const guildSchema = require('../../../schemes/guild');
+			collector.on('collect', async i => {
+				const guildSchema = require('../../../schemes/guild');
 
-                        setLanguage(i.guild, i.values[0]);
+				setLanguage(i.guild, i.values[0]);
 
-                        await guildSchema.findOneAndUpdate(
-                            {
-                                _id: i.guild.id
-                            },
-                            {
-                                _id: i.guild.id,
-                                lang: i.values[0]
-                            },
-                            {
-                                upsert: true
-                            });
+				await guildSchema.findOneAndUpdate(
+					{
+						_id: i.guild.id
+					},
+					{
+						_id: i.guild.id,
+						lang: i.values[0]
+					},
+					{
+						upsert: true
+					});
 
-                        const embed = new MessageEmbed()
-                            .setDescription(translate.commands.lang.changed.replace('${langValue}', i.values[0]))
-                            .setColor(config.color.primary);
+				embed = new MessageEmbed()
+				.setDescription(translate.commands.lang.changed.replace('${1}', i.values[0]))
+				.setColor(config.color.primary);
 
-                        await i.reply({embeds: [embed], ephemeral: true}).catch(console.error);
-                    }
-                });
+				await i.reply({embeds: [embed], ephemeral: true}).catch(console.error);
+			});
 
-                collector.on('end', collected => {
-                    interaction.editReply(translate.errors[1])
-                });
-            }).catch(console.error);
-    },
+			collector.on('end', collected => {
+				interaction.editReply(translate.errors[1]);
+			});
+		}).catch(console.error);
+	}
 };
