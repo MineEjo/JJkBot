6aa9b83278b3afbf4a36c8b74e427cf8c4f36f63
MineEjo <evgenyshaimov@gmail.com>
2022-02-05 17:07:22 +0300

    Added Access Handler.

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,30 +1,48 @@
+const config = require('../data/config.json');
 const language = require('../functions/handleLanguages');
 
 module.exports = {
-    name: 'interactionCreate',
-    async execute(interaction, client) {
-        if (interaction.isCommand()) {
-            const translate = require(`../translation/${language(interaction.guild)}.json`);
-            const command = client.commands.get(interaction.commandName);
+	name: 'interactionCreate',
+	async execute(interaction, client) {
+		if (interaction.isCommand()) {
+			const translate = require(`../translation/${language(interaction.guild)}.json`);
+			const command = client.commands.get(interaction.commandName);
 
-            if (!command) return;
+			if (!command) return;
 
-            try {
-                if (command.permissions && command.permissions.length > 0) {
-                    if (!interaction.member.permissions.has(command.permissions)) return await interaction.reply({
-                        content: translate.errors[2],
-                        ephemeral: true
-                    })
-                }
+			try {
+				if (command.restriction && command.restriction.length > 0) {
+					if (command.restriction === config.FLAGS.CHANNEL && !interaction.guild) return await interaction.reply({
+						content: translate.errors[3],
+						ephemeral: true
+					});
 
-                await command.execute(interaction, client);
-            } catch (error) {
-                console.error(error);
-                await interaction.reply({
-                    content: translate.errors[0],
-                    ephemeral: true
-                });
-            }
-        }
-    },
+					if (command.restriction === config.FLAGS.DMCHANNEL && interaction.guild) return await interaction.reply({
+						content: translate.errors[4],
+						ephemeral: true
+					});
+
+					if (command.restriction === config.FLAGS.BOT_OWNER && interaction.member.id !== config.bot.ownerId) return await interaction.reply({
+						content: translate.errors[5],
+						ephemeral: true
+					});
+				}
+
+				if (command.permissions && command.permissions.length > 0) {
+					if (!interaction.member.permissions.has(command.permissions)) return await interaction.reply({
+						content: translate.errors[2],
+						ephemeral: true
+					});
+				}
+
+				await command.execute(interaction, client);
+			} catch (error) {
+				console.error(error);
+				await interaction.reply({
+					content: translate.errors[0],
+					ephemeral: true
+				});
+			}
+		}
+	}
 };
