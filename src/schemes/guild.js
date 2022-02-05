a28cf9f348945e0249bce99305abd3a84a86d9b8
MineEjo <evgenyshaimov@gmail.com>
2022-02-05 17:14:10 +0300

    Added New Parameters.

--- src/schemes/guild.js
+++ src/schemes/guild.js
@@ -1,14 +1,40 @@
 const mongoose = require('mongoose');
+const config = require('../data/config.json');
 
 const guildSchema = mongoose.Schema({
-    _id: {
-        type: String,
-        required: true
-    },
-    lang: {
-        type: String,
-        required: true
-    }
+	_id: {
+		maxLength: 18,
+		minLength: 18,
+		type: String,
+		required: true
+	},
+	lang: {
+		minLength: 4,
+		maxLength: 4,
+		type: String,
+		default: config.bot.lang
+	},
+	deleteInvites: {
+		minLength: 1,
+		maxLength: 1,
+		type: Number,
+		default: config.settings.off
+	},
+	deleteScamLinks: {
+		minLength: 1,
+		maxLength: 1,
+		type: Number,
+		default: config.settings.off
+	},
+	deleteLinks: {
+		minLength: 1,
+		maxLength: 1,
+		type: Number,
+		default: config.settings.off
+	}
 });
 
+const dataNames = ['deleteInvites', 'deleteScamLinks', 'deleteLinks'];
+
 module.exports = mongoose.model('guild', guildSchema, 'guilds');
+module.exports.dataNames = dataNames;
