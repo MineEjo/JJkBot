e19e9b6166339e0165a6880f60f2bbeef0952cf3
MineEjo <evgenyshaimov@gmail.com>
2022-02-10 09:19:33 +0300

    Mongo Scheme Update.

--- src/schemes/guild.js
+++ src/schemes/guild.js
@@ -1,6 +1,20 @@
 const mongoose = require('mongoose');
 const config = require('../data/config.json');
 
+const dataLangNames = ['lang'];
+
+const dataLinksNames = [
+	'deleteInvites',
+	'deleteScamLinks',
+	'deleteLinks',
+	'hideInvites',
+	'hideLinks'
+];
+
+const dataWhiteLists = [
+	'defaultDsLinks'
+];
+
 const guildSchema = mongoose.Schema({
 	_id: {
 		maxLength: 18,
@@ -8,33 +22,51 @@ const guildSchema = mongoose.Schema({
 		type: String,
 		required: true
 	},
-	lang: {
+	[dataLangNames[0]]: {
 		minLength: 4,
 		maxLength: 4,
 		type: String,
-		default: config.bot.lang
+		default: config?.bot?.lang
 	},
-	deleteInvites: {
+	[dataLinksNames[0]]: {
 		minLength: 1,
 		maxLength: 1,
 		type: Number,
-		default: config.settings.off
+		default: config?.settings?.off
 	},
-	deleteScamLinks: {
+	[dataLinksNames[1]]: {
 		minLength: 1,
 		maxLength: 1,
 		type: Number,
-		default: config.settings.off
+		default: config?.settings?.off
 	},
-	deleteLinks: {
+	[dataLinksNames[2]]: {
 		minLength: 1,
 		maxLength: 1,
 		type: Number,
-		default: config.settings.off
+		default: config?.settings?.off
+	},
+	[dataLinksNames[3]]: {
+		minLength: 1,
+		maxLength: 1,
+		type: Number,
+		default: config?.settings?.off
+	},
+	[dataLinksNames[4]]: {
+		minLength: 1,
+		maxLength: 1,
+		type: Number,
+		default: config?.settings?.off
+	},
+	[dataWhiteLists[0]]: {
+		minLength: 1,
+		maxLength: 1,
+		type: Number,
+		default: config?.settings?.off
 	}
 });
 
-const dataNames = ['deleteInvites', 'deleteScamLinks', 'deleteLinks'];
-
 module.exports = mongoose.model('guild', guildSchema, 'guilds');
-module.exports.dataNames = dataNames;
+module.exports.dataLinksNames = dataLinksNames;
+module.exports.dataLangNames = dataLangNames;
+module.exports.dataWhiteLists = dataWhiteLists;
