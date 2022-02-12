d53a7ee9df85af8c087650a24c9a407dff21a002
MineEjo <evgenyshaimov@gmail.com>
2022-02-12 21:56:01 +0300

    Rewritten Handler Data Guilds

--- src/functions/mongodb/handleGuilds.js
+++ src/functions/mongodb/handleGuilds.js
@@ -1,12 +1,13 @@
 const guildSchema = require('../../schemes/guild');
-const config = require('../../data/config.json');
-const {dataLinksNames} = require('../../schemes/guild');
-const {dataLangNames} = require('../../schemes/guild');
-const {dataWhiteLists} = require('../../schemes/guild');
+const {Data, DataCounts} = require('../../schemes/guild');
 
-const data = [{}, {}, {}, {}, {}, {}, {}];
+const dataCells = [];
 
-const loadGuilds = async (client) => {
+for (let index = 0; index < Data.length; index += DataCounts) {
+	dataCells.push({});
+}
+
+const loadGuilds = async client => {
 	for (const guild of client.guilds.cache) {
 		const guildId = guild[0];
 
@@ -14,64 +15,19 @@ const loadGuilds = async (client) => {
 			_id: guildId
 		});
 
-		data[0][guildId] = result ? result[dataLangNames[0]] : config?.settings?.lang;
-
-		data[1][guildId] = result ? result[dataLinksNames[0]] : config?.settings?.off;
-		data[2][guildId] = result ? result[dataLinksNames[1]] : config?.settings?.off;
-		data[3][guildId] = result ? result[dataLinksNames[2]] : config?.settings?.off;
-		data[4][guildId] = result ? result[dataLinksNames[3]] : config?.settings?.off;
-		data[5][guildId] = result ? result[dataLinksNames[4]] : config?.settings?.off;
-
-		data[6][guildId] = result ? result[dataWhiteLists[0]] : config?.settings?.off;
+		let position = 0;
+		for (let index = 0; index < Data.length; index += DataCounts) {
+			dataCells[position][guildId] = result ? result[Data[index]] : Data[index + 4];
+			position++;
+		}
 	}
 };
 
-const setGuilds = (guild, key, value) => {
-	switch (key) {
-		case dataLangNames[0]:
-			data[0][guild.id] = value;
-			break;
-		case dataLinksNames[0]:
-			data[1][guild.id] = value;
-			break;
-		case dataLinksNames[1]:
-			data[2][guild.id] = value;
-			break;
-		case dataLinksNames[2]:
-			data[3][guild.id] = value;
-			break;
-		case dataLinksNames[3]:
-			data[4][guild.id] = value;
-			break;
-		case dataLinksNames[4]:
-			data[5][guild.id] = value;
-			break;
-		case dataWhiteLists[0]:
-			data[6][guild.id] = value;
-			break;
-	}
+const setGuilds = (guild, data, value) => {
+	dataCells[data.indexOf(data)][guild.id] = value;
 };
 
-module.exports = (guild, key) => {
-	switch (key) {
-		case dataLangNames[0]:
-			return (guild) ? data[0][guild.id] : config?.settings?.lang;
-		case dataLinksNames[0]:
-			return (guild) ? data[1][guild.id] : config?.settings?.off;
-		case dataLinksNames[1]:
-			return (guild) ? data[2][guild.id] : config?.settings?.off;
-		case dataLinksNames[2]:
-			return (guild) ? data[3][guild.id] : config?.settings?.off;
-		case dataLinksNames[3]:
-			return (guild) ? data[4][guild.id] : config?.settings?.off;
-		case dataLinksNames[4]:
-			return (guild) ? data[5][guild.id] : config?.settings?.off;
-		case dataWhiteLists[0]:
-			return (guild) ? data[6][guild.id] : config?.settings?.off;
-		default:
-			return config?.settings?.off;
-	}
-};
+module.exports = (guild, data) => (guild) ? dataCells[data.indexOf(data)][guild.id] : data[data.indexOf(data) + 4];
 
 module.exports.loadGuilds = loadGuilds;
 module.exports.setGuilds = setGuilds;
--- src/schemes/guild.js
+++ src/schemes/guild.js
@@ -1,72 +1,42 @@
 const mongoose = require('mongoose');
 const config = require('../data/config.json');
 
-const dataLangNames = ['lang'];
-
-const dataLinksNames = [
-	'deleteInvites',
-	'deleteScamLinks',
-	'deleteLinks',
-	'hideInvites',
-	'hideLinks'
+const Data = [
+	// name, minLength, maxLength, type, default, required
+	'lang', 4, 4, String, config.settings.lang, false,
+	'deleteLinks', 1, 1, Number, config.settings.off, false,
+	'hideLinks', 1, 1, Number, config.settings.off, false,
+	'allowDefaultLinks', 1, 1, Number, config.settings.off, false,
+	'allowScamLinks', 1, 1, Number, config.settings.off, false,
+	'allowInvites', 1, 1, Number, config.settings.off, false,
+	'allowSocialMedia', 1, 1, Number, config.settings.off, false,
+	'links', null, null, Array, config.settings.voidArray, false,
+	'linksChannels', null, null, Array, config.settings.voidArray, false,
+	'linksRoles', null, null, Array, config.settings.voidArray, false
 ];
 
-const dataWhiteLists = [
-	'defaultDsLinks'
-];
-
-const guildSchema = mongoose.Schema({
+const guildSchema = [{
 	_id: {
 		maxLength: 18,
 		minLength: 18,
 		type: String,
 		required: true
-	},
-	[dataLangNames[0]]: {
-		minLength: 4,
-		maxLength: 4,
-		type: String,
-		default: config?.settings?.lang
-	},
-	[dataLinksNames[0]]: {
-		minLength: 1,
-		maxLength: 1,
-		type: Number,
-		default: config?.settings?.off
-	},
-	[dataLinksNames[1]]: {
-		minLength: 1,
-		maxLength: 1,
-		type: Number,
-		default: config?.settings?.off
-	},
-	[dataLinksNames[2]]: {
-		minLength: 1,
-		maxLength: 1,
-		type: Number,
-		default: config?.settings?.off
-	},
-	[dataLinksNames[3]]: {
-		minLength: 1,
-		maxLength: 1,
-		type: Number,
-		default: config?.settings?.off
-	},
-	[dataLinksNames[4]]: {
-		minLength: 1,
-		maxLength: 1,
-		type: Number,
-		default: config?.settings?.off
-	},
-	[dataWhiteLists[0]]: {
-		minLength: 1,
-		maxLength: 1,
-		type: Number,
-		default: config?.settings?.off
 	}
-});
+}];
 
-module.exports = mongoose.model('guild', guildSchema, 'guilds');
-module.exports.dataLinksNames = dataLinksNames;
-module.exports.dataLangNames = dataLangNames;
-module.exports.dataWhiteLists = dataWhiteLists;
+const DataCounts = 6;
+for (let index = 0; index < Data.length; index += DataCounts) {
+	guildSchema.push({
+		[Data[index]]: {
+			maxLength: Data[index + 1],
+			minLength: Data[index + 2],
+			type: Data[index + 3],
+			default: Data[index + 4],
+			required: Data[index + 5]
+		}
+	});
+}
+
+module.exports = mongoose.model('guild', mongoose.Schema(guildSchema), 'guilds');
+module.exports.Data = Data;
+module.exports.DataCounts = DataCounts;
