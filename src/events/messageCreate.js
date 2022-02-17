737f06fb4162abc740ec49988ffe74d329ebab27
MineEjo <e>
2022-02-17 21:21:33 +0300

    Rewritten auto-moderation for links.

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,9 +1,70 @@
-// const {links} = require('./messageCreate/moderation/links');
+const getDataGuild = require('../functions/mongodb/handleGuilds');
+const config = require('../data/config.json');
+const {getWebhook} = require('../functions/lites/getWebhook');
 
 module.exports = {
 	name: 'messageCreate',
 	once: false,
 	async execute(message) {
-		// await links(message);
+		const translate = require(`../translation/${getDataGuild(message?.guild, 'lang')}.json`);
+
+		if (message && !message?.author.bot && message?.guild && message?.deletable) {
+			let content = message?.content;
+
+			const dataDeleteLinks = getDataGuild(message?.guild, 'deleteLinks');
+			const dataHideLinks = getDataGuild(message?.guild, 'hideLinks');
+
+			if (dataDeleteLinks === config.settings.on || dataHideLinks === config.settings.on) {
+				let linksCount = 0;
+
+				const links = message?.content.match(/(http(s?):\/\/(\S+\.)+\S+|www\d?\.(\S+\.)+\S+)|(discord\.gg\/(\S+)+\S+)|(discordapp\.com\/(\S+)+\S+)/gm);
+				if (!links) {
+					return;
+				}
+
+				let whitelist = [];
+				// Collecting all the white lists into one array of strings.
+				getDataGuild(message?.guild, 'allowDefaultLinks') === config.settings.on ? whitelist = whitelist.concat(require('../data/whitelists/links.json')) : null;
+				getDataGuild(message?.guild, 'allowScamLinks') === config.settings.on ? whitelist = whitelist.concat(require('../data/whitelists/scamLinks.json')) : null;
+				getDataGuild(message?.guild, 'allowInvites') === config.settings.on ? whitelist = whitelist.concat(require('../data/whitelists/invites.json')) : null;
+				getDataGuild(message?.guild, 'allowSocialMedia') === config.settings.on ? whitelist = whitelist.concat(require('../data/whitelists/socialMedia.json')) : null;
+
+				for (const link of links) {
+					// If the link isn't on the whitelist.
+					if (!whitelist.some(word => (link.replace('https://', '').replace('http://', '').slice(0, word.length)) === word)) {
+
+						// If the link is an invitation, check whether the invitation is from this server.
+						if ((link.includes('https://discord.gg') || link.includes('https://discordapp.com')) &&
+							(await message?.guild?.me?.client.fetchInvite(link)).guild?.id === message?.guild?.id) {
+							return
+						}
+
+						if (dataDeleteLinks === config?.settings?.on) {
+							content = content.replace(link, translate?.events?.messageCreate?.words[0]);
+						} else if (dataHideLinks === config?.settings?.on) {
+							content = content.replace(link, `||\`${link}\`||`);
+						}
+
+						linksCount++;
+					}
+				}
+
+				linksCount > 0 ? await authorWebhookSendMessage(content) : null;
+			}
+
+			async function authorWebhookSendMessage(c) {
+				if (message.channel.isText() && !message.channel.isThread()) {
+					const webhook = await getWebhook(message?.guild, message?.channel);
+
+					webhook.send({
+						content: c,
+						username: `${message?.author?.tag} (${message?.author?.id})`,
+						avatarURL: message?.author.avatarURL({dynamic: true})
+					});
+
+					message.delete().catch(console.error);
+				}
+			}
+		}
 	}
 };
