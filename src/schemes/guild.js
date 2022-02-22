53fe703c578ec546d4edeebd1f53d96c39c1cdd8
MineEjo <e>
2022-02-23 00:06:33 +0300

    Added new parameters in the scheme, for logs.
    Signed-off-by: MineEjo <e>

--- src/schemes/guild.js
+++ src/schemes/guild.js
@@ -13,7 +13,9 @@ const Data = [
 	'allowSocialMedia', 1, 1, Number, SETTINGS.OFF, false,
 	'links', null, null, Array, SETTINGS.VOID_ARRAY, false,
 	'linksChannels', null, null, Array, SETTINGS.VOID_ARRAY, false,
-	'linksRoles', null, null, Array, SETTINGS.VOID_ARRAY, false
+	'linksRoles', null, null, Array, SETTINGS.VOID_ARRAY, false,
+	'logs', 1, 1, Number, SETTINGS.OFF, false,
+	'logsChannel', 18, 18, String, SETTINGS.VOID_STRING, false
 ];
 
 const guildSchema = [{
