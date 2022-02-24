26b3d8d6c072c82c4a956a32b7da9a5ac36a27cb
Evgeny <98797719+mineejo@users.noreply.github.com>
2022-02-24 17:37:54 +0300

    Added access restriction - server.
    Signed-off-by: MineEjo <e>

--- src/commands/global/information/help.js
+++ src/commands/global/information/help.js
@@ -22,6 +22,7 @@ import {MessageEmbed} from 'discord.js';
 import SETTINGS from '../../../data/enums/settings.json';
 import COLORS from '../../../data/enums/colors.json';
 import LINKS from '../../../data/enums/links.json';
+import FLAGS from '../../../data/enums/flags.json';
 
 import getDataGuild from '../../../functions/mongodb/handleGuilds.js';
 
@@ -31,6 +32,7 @@ export default {
 	data: new SlashCommandBuilder()
 	.setName(noneTranslate?.commands?.help?.slash?.name)
 	.setDescription(noneTranslate?.commands?.help?.slash?.description),
+	restriction: FLAGS.CHANNEL,
 	async execute(interaction) {
 		const translate = (await import(`../../../translation/${getDataGuild(interaction?.guild, 'lang')}.json`)).default;
 
